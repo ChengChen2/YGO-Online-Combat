97fd47718d94f5a2ab2bbe7cea425107668cdb8d
unknown <2041757928@qq.com>
2021-01-26 17:40:47 +0800

    code change

--- ply1/js/game-control.js
+++ ply1/js/game-control.js
@@ -1,11 +1,10 @@
 
 var P1DeckName = "Deck_KaiMa";  //我方牌组名
 var P1DeckNum = 41;  //我方牌组卡片数量
-var P1DeckPointer = 0; //卡组当前卡片指针
 
 var P1Deck = [];  //储存我方牌组所有卡片src
 
-/*-----------------游戏逻辑控制部分-------------------*/
+/*-----------------游戏控制逻辑部分-------------------*/
 
 //储存P1卡组所有卡片路径
 for (var i=0; i<P1DeckNum; i++) {
@@ -17,15 +16,17 @@ for (var i=0; i<P1DeckNum; i++) {
 P1Deck = shuffle(cloneArr(P1Deck));
 //console.log(P1Deck);
 
+// 获取空的img src路径，方便其他函数判断卡槽是否为空
+// window.onload 使函数在html完全加载后执行
 var emptysrc;
 window.onload = function() {
-  var handID = 'p1-hand0';
+  var handID = 'p1-field0';
   element = document.getElementById(handID);
   emptysrc = element.src;
 }
 
 
-/*----------------各类控制函数-------------------------*/
+/*-----------------各类控制函数-------------------------*/
 
 /*生成随机数*/
 function getRandom(min, max) {
@@ -59,7 +60,6 @@ function drawCard() {
         element = document.getElementById(handID);
         if (element.src == emptysrc) {
           element.src = P1Deck.pop();
-          //console.log(P1Deck);
           break;
         }
     }
